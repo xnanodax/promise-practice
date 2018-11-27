3ec41652208f609e40cead0cb1c5390a4af0a9a9
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-11-26 21:14:26 -0800

    Return waits to search for movie titles from all pageIdx. Update code system

--- index.js
+++ index.js
@@ -2,36 +2,37 @@ const https = require('https')
 
 function getAllMoviesData(substr) {
   const titles = [];
-  let pageIdx = 1;
-  return new Promise((resolve, reject) => {
-    
+  const pageIdx = 1;
+  return new Promise((resolveOuter, rejectOuter) => {
+  
     const _getAllMoviesData = function (substr, pageIdx) {
-      console.log("9", pageIdx)
-      const url = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageIdx}`;
-      let rawData = '';
-      
-      const getCallback = function(response) {
-        response.on('data', function(data) {
-          rawData += data;
-        });
+      return new Promise((resolveInner, rejectOuter) => {
+        const url = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageIdx}`;
+        let rawData = '';
         
-        response.on('end', function() {
-          const parsedData = JSON.parse(rawData);
-          const movies = parsedData.data;
-          console.log("21", pageIdx)
-          movies.forEach(movie => titles.push(movie.Title))
+        const getCallback = function(response) {
+          response.on('data', data => rawData += data);
           
-          if (pageIdx < parsedData.total_pages) {
-            pageIdx += 1;
-            _getAllMoviesData(substr, pageIdx);
-            resolve(titles);
-          }
-        });
-      }
-      https.get(url, getCallback);
+          response.on('end', () => {
+            const parsedData = JSON.parse(rawData);
+            const movies = parsedData.data;
+            movies.forEach(movie => titles.push(movie.Title))
+            
+            if (pageIdx < parsedData.total_pages) {
+              _getAllMoviesData(substr, pageIdx + 1)
+                .then(() => resolveInner(titles));
+            } 
+            resolveInner(titles);
+
+          });
+        }
+        
+        https.get(url, getCallback);
+      })
     }
-    _getAllMoviesData(substr, pageIdx);
-    return titles;
+
+    _getAllMoviesData(substr, pageIdx).then(result => resolveOuter(result));
+
   });
 };
 getAllMoviesData('spiderman').then((result)=> console.log("Movie Titles: ", result));
