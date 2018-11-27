9240ac725831ae14c1b7ec3c02e8ec236a5f5615
Cynthia Ma <cynthia.ma.emily@gmail.com>
2018-11-27 09:47:59 -0800

    Fixed async/await bug after the first page

--- indexWithAsyncAwait.js
+++ indexWithAsyncAwait.js
@@ -3,32 +3,30 @@ const https = require('https');
 async function getAllMoviesData(substr) {
   const titles = [];
   const pageIdx = 1;
-  const ajaxCall = async () => new Promise((resolveOuter) => {
+  const ajaxCall = async () => new Promise(resolveOuter => {
 
-    const _getAllMoviesData = function (substr, pageIdx) {
-      return new Promise((resolveInner) => {
+    const _getAllMoviesData = async (substr, pageIdx) => {
+      return new Promise(resolveInner => {
         const url = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}&page=${pageIdx}`;
         let rawData = '';
-        
-        const getCallback = function(response) {
+
+        const getCallback = async function(response) {
           response.on('data', data => rawData += data);
-          
-          response.on('end', () => {
+          response.on('end', async () => {
             const parsedData = JSON.parse(rawData);
             const movies = parsedData.data;
             movies.forEach(movie => titles.push(movie.Title))
             
             if (pageIdx < parsedData.total_pages) {
-              _getAllMoviesData(substr, pageIdx + 1)
+              await _getAllMoviesData(substr, pageIdx + 1)
             }
-            
             resolveInner(titles);
           });
         }
         
         https.get(url, getCallback);
-      })
-    }
+      });
+    };
 
     const result = _getAllMoviesData(substr, pageIdx);
     resolveOuter(result);
